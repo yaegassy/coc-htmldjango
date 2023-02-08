7029766df4deee6421ce52d15d17e0c84987b374
yaegassy <yaegassy@users.noreply.github.com>
2023-02-08 13:16:49 +0900

    refactor: use pyproject.toml in the built-in installer (#20)

--- src/constant.ts
+++ src/constant.ts
@@ -1,15 +1,28 @@
-/**
- * System-wide constants.
- *
- * May be read from package.json
- */
+import fs from 'fs';
+import path from 'path';
+import toml from 'toml';
 
-// @ts-ignore
-import { djlintVersion } from '../package.json';
-// @ts-ignore
-import { djhtmlVersion } from '../package.json';
+type PyprojectToml = {
+  tool: {
+    poetry: {
+      dependencies: {
+        [name: string]: string;
+      };
+    };
+  };
+};
+
+function getPackageVersion(name: string) {
+  const rootDir = path.resolve(path.dirname(__filename), '..');
+  const filePath = path.join(rootDir, 'pyproject.toml');
+  const fileStr = fs.readFileSync(filePath);
+  const data: PyprojectToml = toml.parse(fileStr);
+  const version = data.tool.poetry.dependencies[name];
+
+  return version;
+}
+
+export const DJHTML_VERSION = getPackageVersion('djhtml');
+export const DJLINT_VERSION = getPackageVersion('djlint');
 
 export const SUPPORT_LANGUAGES = ['htmldjango'];
-
-export const DJHTML_VERSION = djhtmlVersion;
-export const DJLINT_VERSION = djlintVersion;
